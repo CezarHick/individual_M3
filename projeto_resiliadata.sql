4e726d81595173f643bb6bf4733ce9221942695c
Cezar Henrique <132731147+CezarHick@users.noreply.github.com>
2023-12-01 14:59:02 -0300

    Update projeto_resiliadata.sql

--- projeto_resiliadata.sql
+++ projeto_resiliadata.sql
@@ -3,7 +3,7 @@ USE RESILIADATA;
 
 CREATE TABLE empresa 
 ( 
-    id_empresa INT PRIMARY KEY AUTO_INCREMENT,  
+    id_empresa INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,  
     nome_empresa VARCHAR(100) NOT NULL,  
     endereco VARCHAR(100) NOT NULL,  
     telefone VARCHAR(15) NOT NULL
@@ -11,7 +11,7 @@ CREATE TABLE empresa
 
 CREATE TABLE colaborador
 ( 
-    id_colaborador INT PRIMARY KEY AUTO_INCREMENT,  
+    id_colaborador INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,  
     nome VARCHAR(100) NOT NULL,  
     cargo VARCHAR(100) NOT NULL,  
     id_empresa INT NOT NULL 
@@ -19,16 +19,17 @@ CREATE TABLE colaborador
 
 CREATE TABLE tecnologias
 ( 
-    id_tecnologias INT PRIMARY KEY AUTO_INCREMENT,  
+    id_tecnologias INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  
     area VARCHAR(100) NOT NULL,  
     nome_tecnologia VARCHAR(100) NOT NULL
 ); 
 
 CREATE TABLE tecnologia_empresa 
 ( 
+    id_emp_tecnologia INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  
     id_empresa INT NOT NULL,  
     id_tecnologias INT NOT NULL,  
-    nivel_utilizacao INT NOT NULL  -- 1 a 5 (Sendo 1 pouco usado e 5 muito usado)
+    nivel_utilizacao INT NOT NULL  
 ); 
 
 
@@ -52,9 +53,9 @@ VALUES
 ('Will Smith', 'Developer', 1),
 ('Taylor Swift', 'Data Scientist', 2);
 
-INSERT INTO tecnologia_empresa(id_empresa, id_tecnologias, nivel_utilizacao) 
+INSERT INTO tecnologia_empresa(id_empresa, id_tecnologias, nivel_utilizacao, id_emp_tecnologia) 
 VALUES 
-(1, 1, 4),
-(1, 2, 3),
-(2, 1, 5),
-(2, 2, 2);
+(1, 1, 1, 4),
+(2, 1, 2, 3),
+(3, 2, 1, 5),
+(4, 2, 2, 2);
